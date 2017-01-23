b0ad6bcdcd96e55713f0adf2abebd8fcf23a945b
Guorblam <tholeguen@protonmail.com>
2017-01-23 18:30:23 +0100

    Branche de tests : ajout des requirements de test et initialisation

---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 21/01/2017
+ * Time: 10:48
+ */
+
+namespace Test\AppBundle\Controller;
+
+class UserControllerTest extends \PHPUnit_Framework_TestCase
+{
+}
