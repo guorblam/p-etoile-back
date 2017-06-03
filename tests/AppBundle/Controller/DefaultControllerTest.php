f893ccd25942c037575a4a104331d2b25b244df8
Guorblam <tholeguen@protonmail.com>
2017-06-03 16:22:55 +0200

    standardisation de la configuration swift mailer + cas de test exemple d'utilisation dans le default controller.

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -8,11 +8,24 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
+    }
+
+    /**
+     * Test des emails. Pour contrôler l'envoi/réception, changer le paramètre swiftmailer -> disable_delivery à false
+     */
+    public function testEmail()
+    {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $container = $client->getContainer();
+        $mailer = $container->get('swiftmailer.mailer');
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setFrom('tdev7635@gmail.com')
+            ->setTo('tholeguen@gmail.com')
+            ->setBody(
+                "Ey"
+            );
+        $result = $mailer->send($message);
+        $this->assertTrue($result === 1);
     }
 }
